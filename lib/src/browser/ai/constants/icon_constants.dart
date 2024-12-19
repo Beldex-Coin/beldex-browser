da0b0ddafe36e5ad91d8bcf345fc289b19252970
Gabsid <gabsid@beldex.io>
2024-12-19 21:01:55 +0530

    Add: Assets and string file

---
+++ lib/src/browser/ai/constants/icon_constants.dart
@@ -0,0 +1,19 @@
+
+class IconConstants{
+
+  static const String browserAITransparentPng = "assets/images/ai-icons/browser_ai_transparent.png";
+  static const String beldexAILogoSvg =  'assets/images/ai-icons/beldex_ai.svg';
+  static const String closeIconDark = "assets/images/ai-icons/clear_dark.svg";
+  static const String closeIconWhite = "assets/images/ai-icons/clear.svg";
+  static const String welcomeBeldexAIDark = "assets/images/ai-icons/welcome_ai_dark.svg";
+  static const String welcomeBeldexAIWhite = "assets/images/ai-icons/welcome_ai_white.svg";
+  static const String micDark = "assets/images/ai-icons/mic_dark.svg";
+  static const String micWhite = "";
+  static const String sendDark = "assets/images/ai-icons/send_dark.svg";
+  static const String summariseIcon = "assets/images/ai-icons/summarise_page.svg";
+  static const String bubbleLoaderDark = "assets/images/ai-icons/bubble_dark_theme.json";
+  static const String bubbleLoaderWhite = "assets/images/ai-icons/bubble_white_theme.json";
+  static const String copyIconDark = "assets/images/ai-icons/Copy_dark.svg";
+  static const String copyIconWhite = "assets/images/ai-icons/Copy 1-1.svg";
+
+}
---
+++ lib/src/browser/ai/string_constants.dart
@@ -0,0 +1,10 @@
+
+class StringConstants{
+  static const String welcomeAIContent = "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor.";
+  static const String needHelpWithSite = "Need help with website?";
+  static const String iCanHelpYouSummarising = "I can help you summarising articles, expanding on a site's content and much more. Not sure where to start? Try this:";
+  static const String summariseThispage = "Summarise this page";
+  static const String enterPromptHere = "Enter prompt here..";
+  static const String beldexAI = "Beldex AI";
+  static const String hideSummarise = "Hide Summarise";
+}
