2accea3c9ad21f1480cb5eafd27a5ec0c10fcfe5
Gabsid <gabsid@beldex.io>
2025-01-31 16:42:26 +0530

    Domain urls added

--- lib/src/browser/webview_tab.dart
+++ lib/src/browser/webview_tab.dart
@@ -47,6 +47,27 @@ class _WebViewTabState extends State<WebViewTab> with WidgetsBindingObserver {
 
   List<ContentBlocker>? contentBlockers = []; // Domain filter variable for ad blocks
 
+
+   final adUrlFilters = [  
+    ".\*.doubleclick.net/.\*",  
+    ".\*.ads.pubmatic.com/.\*",  
+    ".\*.googlesyndication.com/.\*",  
+    ".\*.google-analytics.com/.\*",  
+    ".\*.adservice.google.\*/.\*",  
+    ".\*.adbrite.com/.\*",  
+    ".\*.exponential.com/.\*",  
+    ".\*.quantserve.com/.\*",  
+    ".\*.scorecardresearch.com/.\*",  
+    ".\*.zedo.com/.\*",  
+    ".\*.adsafeprotected.com/.\*",  
+    ".\*.teads.tv/.\*",  
+    ".\*.outbrain.com/.\*"  
+  ];
+
+
+
+
+
   @override
   void initState() {
     WidgetsBinding.instance.addObserver(this);
