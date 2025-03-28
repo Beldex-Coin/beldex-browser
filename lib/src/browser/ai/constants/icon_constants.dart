62246f54fabc5067e4d0213f66a2eb0f97f896c2
Gabsid <gabsid@beldex.io>
2025-03-28 16:29:05 +0530

    Icons file updated

--- lib/src/browser/ai/constants/icon_constants.dart
+++ lib/src/browser/ai/constants/icon_constants.dart
@@ -9,11 +9,12 @@ class IconConstants{
   static const String welcomeBeldexAIWhite = "assets/images/ai-icons/welcome_ai_white.svg";
   static const String micDark = "assets/images/ai-icons/mic_dark.svg";
   static const String micWhite = "";
-  static const String sendDark = "assets/images/ai-icons/send_dark.svg";
+  static const String send = "assets/images/ai-icons/send_green.svg";
   static const String sendWhite = "assets/images/ai-icons/send.svg";
   static const String summariseIcon = "assets/images/ai-icons/summarise_page.svg";
   static const String bubbleLoaderDark = "assets/images/ai-icons/bubble_dark_theme.json";
   static const String bubbleLoaderWhite = "assets/images/ai-icons/bubble_white_theme.json";
+  static const String shareIcon = "assets/images/ai-icons/Shares.svg";
   static const String copyIconDark = "assets/images/ai-icons/Copy_dark.svg";
   static const String copyIconWhite = "assets/images/ai-icons/Copy 1-1.svg";
   static const String beldexAILogoWhiteColor = 'assets/images/ai-icons/beldex_ai_white.svg';
